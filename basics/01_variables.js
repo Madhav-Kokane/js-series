198f25363e4f3df4923fa72f8e836849f47f6e2e
Madhav-Kokane <kokanemadhav4@gmail.com>
2025-08-23 10:32:36 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,21 @@
+console.log("Variables")
+const accountId=42;
+let accountMail="madhav@google.com";
+var accountPassward="1234";
+let accountState;
+
+//We Can Directly reserve memory for any variable without using any Keyword in JS 
+//But Not an proper Way
+accountCity="Nanded";
+
+//Const can not be updated 
+// accountId=40;
+console.log("Account Id = ",accountId);
+
+//New Method to print Data in Table Format
+console.table([accountId,accountMail,accountPassward,accountCity,accountState]);
+
+/*
+  Prefer Not to Use (Var)
+  Because of Issue in Block Scope and Functional Scopes 
+*/
