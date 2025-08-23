2f6f5028d2f66d7213cd3ec9c0d64a9962d2010c
Madhav-Kokane <kokanemadhav4@gmail.com>
2025-08-23 10:47:03 +0000

    02_DataTypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //Treates all Js Code as A Newer Version 
+
+//alert(3+3); // Gives an Error Bcz We are using nodeJs not Browser It is possible on Browser 
+console.log(3 +
+    3
+);//Not a Optimal Approach Bcz Code Tradiability Should Be High 
+
+/*
+1) Numeric Datatypes 
+   number =>  
+   bigint
+
+2) Non Numeric
+   string => ""
+   boolean => stores true/false
+   NULL => Standanlone value 
+   undefined
+   symbol => Uniqueness    
+*/
+
+//Non-Premitive data Types 
+
+/*
+a) Object
+b) arrays 
+c) functions
+d)Data Object
+5)Regular Expression
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
