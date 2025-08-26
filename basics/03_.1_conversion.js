5f38da593318a675aa8cc9717677ff2b662aad87
Madhav-Kokane <kokanemadhav4@gmail.com>
2025-08-26 13:22:26 +0000

    Confusion in String Conversions

---
+++ basics/03_.1_conversion.js
@@ -0,0 +1,40 @@
+let val=5;
+let negVal=-val;
+// console.log(negVal);
+
+let str1="Hello";
+let str2=" Madhav";
+let str3=str1+str2;
+console.log(str3);
+
+//String Conversion Conversion 
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);  //op-> 122
+/*evha String ne start hote 
+tevha sagla string consider karun aconcatenate karete*/
+
+console.log(1+2+"2");  //op->32
+/*
+But jevha don integer chya operations 
+ne suravat hote tevha operation perform hoto
+tyacha output + string concatenate Hovun Ans yete */ 
+
+console.log(true); //true
+console.log(+true); //1 : converts into true
+
+console.log(+""); // 0 : since empty string converts as 0
+
+let gameCounter=100;
+// console.log(++gameCounter); //101
+console.log(gameCounter++); //100
+
+
+
+
+
+
+
+
+
